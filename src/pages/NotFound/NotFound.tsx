c68d6d6e6a72a875381a1ea498f516b0af333459
vandelay87 <akliaissat@outlook.com>
2025-06-22 08:50:43 +0100

    not found styling

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold text-red-500">404 - Page Not Found</h1>
+    <section>
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 dark:text-white mb-4">404 - Page Not Found</h1>
       <p>Oops! The page you are looking for does not exist.</p>
-    </div>
+    </section>
   )
 }
